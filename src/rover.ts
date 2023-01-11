ac1cd605e87da4ee222631e3275ebd2558a2a7f0
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2023-01-11 19:25:47 +0000

    added static property plateau of class grid and improved the constructor to check the rover is within the plateau limits

--- src/rover.ts
+++ src/rover.ts
@@ -1,17 +1,22 @@
-
-
+import { Grid } from "./grid";
+import { setPlateauSizeCmd } from "./interface_controller";
 import { RoverPosition } from "./types";
 
-
 export class Rover {
-    position : RoverPosition; 
-   
-    constructor(position : RoverPosition) {
-      this.position = position;
-    }
+  static plateau = new Grid();
 
-    getPosition () {
-      return this.position;
+  position: RoverPosition;
+
+  constructor(position: RoverPosition) {
+    if (Rover.plateau.isWithin(position)) {
+      console.log(" *** Rover : Is withingrid => ",Rover.plateau.isWithin(position) )
+      this.position = position;
+    } else {
+      this.position = { x: 0, y: 0, facing: position.facing };
     }
-    
   }
+
+  getPosition() {
+    return this.position;
+  }
+}
