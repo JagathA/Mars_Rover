3f921dfe3df2bb612466ab0c8fc709d106c509a0
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2023-01-11 19:22:29 +0000

    made size private. added getSize and SetSize and isWithin methods

--- src/grid.ts
+++ src/grid.ts
@@ -1,11 +1,24 @@
-
-import { GridSize } from "./types";
-
+import { GridSize, XYPosition } from "./types";
 
 export class Grid {
-    size : GridSize;  
-   
-    constructor(maxX : number , maxY : number) {
-      this.size = {x:maxX, y:maxY};
-    }
+  private size: GridSize;
+
+  constructor(maxX: number=0, maxY: number=0) {
+    this.size = { x: maxX, y: maxY };
   }
+
+  setSize(size:GridSize){
+    this.size = size;
+  }
+  
+  getSize():GridSize{
+    return (this.size)
+  }
+
+  isWithin(position: XYPosition): boolean {
+    console.log("*****rover  postion =>",position );
+    console.log("*****grid  size =>",this.size )
+
+    return position.x < this.size.x && position.y < this.size.y;
+  }
+}
