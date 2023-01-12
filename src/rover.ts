6fc8c3ffc6924b7651d5d398dacfcd22cf966ed0
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2023-01-12 14:26:10 +0000

    added code to handle the move on xy plane

--- src/rover.ts
+++ src/rover.ts
@@ -1,19 +1,16 @@
 import { Grid } from "./grid";
 import { setPlateauSizeCmd } from "./interface_controller";
-import { RoverPosition, Move, Direction, Rotate } from "./types";
+import { XYPosition, RoverPosition, Move, Direction, Rotate } from "./types";
 
 export class Rover {
   static plateau = new Grid();
 
   private readonly rotateDirn: Direction[] = ["N", "E", "S", "W"];
-  private readonly rotateLeftDirn: Direction[] = ["N", "W", "S", "E"];
-  //[["N", "E"], ["E", "S"], ["S", "W"],["W", "N"]]
 
   position: RoverPosition;
 
   constructor(position: RoverPosition) {
     if (Rover.plateau.isWithin(position)) {
-      //console.log(" *** Rover : Is withingrid => ",Rover.plateau.isWithin(position) )
       this.position = position;
     } else {
       this.position = { x: 0, y: 0, facing: position.facing };
@@ -29,23 +26,54 @@ export class Rover {
     cmd.forEach((step) => {
       if (step === "L" || step === "R") {
         this.rotate(step);
+      } else {
+        this.moveXY();
       }
-      // else {move()};
     });
     return this.position;
   }
 
   private rotate(dirn: Rotate) {
     let rotateOffset: number = 0;
-    
-    if (dirn === "R") {      // *** can improve here
+
+    if (dirn === "R") {
+      // *** can improve here
       rotateOffset = 1;
     } else {
       rotateOffset = this.rotateDirn.length - 1;
     }
 
-    let index = this.rotateDirn.indexOf(this.position.facing);
-    index = (index + rotateOffset) % this.rotateDirn.length;
-    this.position.facing = this.rotateDirn[index];
+    let currentDirnIndex = this.rotateDirn.indexOf(this.position.facing);
+    currentDirnIndex =
+      (currentDirnIndex + rotateOffset) % this.rotateDirn.length;
+    this.position.facing = this.rotateDirn[currentDirnIndex];
+  }
+
+  private moveXY() {
+    let currentPosition: XYPosition = {
+      x: this.position.x,
+      y: this.position.y,
+    };
+
+    switch (this.position.facing) {
+      case "N":
+        currentPosition.y++;
+        break;
+      case "E":
+        currentPosition.x++;
+        break;
+      case "S":
+        currentPosition.y--;
+        break;
+      case "W":
+        currentPosition.x--;
+        break;
+      default:
+      //default block statement;
+      //** raise error here */
+    }
+
+    this.position.x = currentPosition.x;
+    this.position.y = currentPosition.y;
   }
 }
