4a5b6627f06e7d64029baeae622cb1506c477aae
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2023-01-12 17:53:26 +0000

    efactoring -Removed CurrentRover and now uses getCurrentRover() and setCurrentRover

--- src/interface_controller.ts
+++ src/interface_controller.ts
@@ -1,23 +1,18 @@
 import { Grid } from "./grid";
 import { Rover } from "./rover";
-//type Coords = number;
-// type XCoords = Coords;
-// type YCoords = Coords;
 
 import {XYPosition,RoverPosition, GridSize, Move} from "./types"
-
-let currentRover : Rover;  // Bad !need to remove this global varaiable later
 
 export function setPlateauSizeCmd (size :GridSize){
     Rover.plateau.setSize(size);
 }
 
 export function placeRoverOnMarsCmd(position : RoverPosition) : RoverPosition{
-    currentRover = new Rover(position);
+    const currentRover = new Rover(position);
+    Rover.SetCurrentRover(currentRover);
     return currentRover.getPosition();
 }
 
 export function moveRoverCmd(cmd : Move[]) : RoverPosition{
-   
-    return currentRover.move(cmd);
+    return Rover.getCurrentRover().move(cmd);
 }
